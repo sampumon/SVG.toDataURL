2a36112fcdeb98d704ff285e42cae30680297476
sampumon <samuli.kaipiainen@helsinki.fi>
2012-06-03 19:57:37 +0300

    api refactored, native hack works with latest firefox via callback

--- svg_todataurl.js
+++ svg_todataurl.js
@@ -1,43 +1,57 @@
 /**
-	The missing SVG.toDataURL library for your SVG elements
+	The missing SVG.toDataURL library for your SVG elements.
 	
-	SVG.toDataURL( [type], [keepNonSafe=false], [keepOutsideViewport=false] )
+	Usage: SVGElement.toDataURL( type, { options } )
+
+	Returns: the data URL, except when using native PNG renderer (needs callback).
 
 	type	MIME type of the exported data.
 			Default: image/svg+xml.
 			Must support: image/png.
-	
-	[the rest of the parameters only apply when exportin image/png (or other non-svg)]
 
-	keepNonSafe
-		Export non-safe (image and foreignObject) elements.
-		This will set the Canvas origin-clean property to false, if this data is transferred to Canvas.
-		Default: false (to keep origin-clean true).
-
-	keepOutsideViewport
-		Export all drawn content, even if not visible.
-		Default: false, export only visible viewport, similar to Canvas toDataURL().
+	options is a map of options: {
+		callback: function(dataURL)
+			Callback function which is called when the data URL is ready.
+			This is only necessary when using native PNG renderer.
+			Default: undefined.
 		
-	IMPLEMENTATION NOTES
-	
-	keepNonSafe and keepOutsideViewport are not supported at all and will be ignored.
-	
-	if you don't have canvg, a client-side hack¹ is attempted, 
-	but this will fail on all current browsers (as of 2010-08)
-	
+		[the rest of the options only apply when type="image/png"]
+
+		renderer: "native"|"canvg"
+			PNG renderer to use. Native renderer¹ probably causes security exception.
+			Default: canvg if available, otherwise native.
+
+		keepNonSafe: true|false
+			Export non-safe (image and foreignObject) elements.
+			This will set the Canvas origin-clean property to false, if this data is transferred to Canvas.
+			Default: false, to keep origin-clean true.
+			NOTE: not currently supported and is just ignored.
+
+		keepOutsideViewport: true|false
+			Export all drawn content, even if not visible.
+			Default: false, export only visible viewport, similar to Canvas toDataURL().
+			NOTE: only supported with canvg renderer.
+	}
+
+	See original paper¹ for more info on SVG to Canvas exporting.
+
 	¹ http://svgopen.org/2010/papers/62-From_SVG_to_Canvas_and_Back/#svg_to_canvas
 */
 
-SVGElement.prototype.toDataURL = function(type, keepNonSafe, keepOutsideViewport) {
+SVGElement.prototype.toDataURL = function(type, options) {
 	var _svg = this;
 	
 	function debug(s) {
-		console.log("SVG.toDataURL: " + s);
+		console.log("SVG.toDataURL:", s);
 	}
 
 	function exportSVG() {
 		var svg_xml = XMLSerialize(_svg);
 		var svg_dataurl = base64dataURLencode(svg_xml);
+		debug(type + " length: " + svg_dataurl.length);
+
+		// NOTE double data carrier
+		if (options.callback) options.callback(svg_dataurl);
 		return svg_dataurl;
 	}
 
@@ -78,170 +92,43 @@ SVGElement.prototype.toDataURL = function(type, keepNonSafe, keepOutsideViewport
 	
 	}
 
-
-	var Base64 = {
-
-		// private property
-		_keyStr : "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
-
-		// public method for encoding
-		encode : function (input) {
-			var output = "";
-			var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
-			var i = 0;
-
-			input = Base64._utf8_encode(input);
-
-			while (i < input.length) {
-
-				chr1 = input.charCodeAt(i++);
-				chr2 = input.charCodeAt(i++);
-				chr3 = input.charCodeAt(i++);
-
-				enc1 = chr1 >> 2;
-				enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
-				enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
-				enc4 = chr3 & 63;
-
-				if (isNaN(chr2)) {
-					enc3 = enc4 = 64;
-				} else if (isNaN(chr3)) {
-					enc4 = 64;
-				}
-
-				output = output +
-				this._keyStr.charAt(enc1) + this._keyStr.charAt(enc2) +
-				this._keyStr.charAt(enc3) + this._keyStr.charAt(enc4);
-
-			}
-
-			return output;
-		},
-
-		// public method for decoding
-		decode : function (input) {
-			var output = "";
-			var chr1, chr2, chr3;
-			var enc1, enc2, enc3, enc4;
-			var i = 0;
-
-			input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
-
-			while (i < input.length) {
-
-				enc1 = this._keyStr.indexOf(input.charAt(i++));
-				enc2 = this._keyStr.indexOf(input.charAt(i++));
-				enc3 = this._keyStr.indexOf(input.charAt(i++));
-				enc4 = this._keyStr.indexOf(input.charAt(i++));
-
-				chr1 = (enc1 << 2) | (enc2 >> 4);
-				chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
-				chr3 = ((enc3 & 3) << 6) | enc4;
-
-				output = output + String.fromCharCode(chr1);
-
-				if (enc3 != 64) {
-					output = output + String.fromCharCode(chr2);
-				}
-				if (enc4 != 64) {
-					output = output + String.fromCharCode(chr3);
-				}
-
-			}
-
-			output = Base64._utf8_decode(output);
-
-			return output;
-
-		},
-
-		// private method for UTF-8 encoding
-		_utf8_encode : function (string) {
-			string = string.replace(/\r\n/g,"\n");
-			var utftext = "";
-
-			for (var n = 0; n < string.length; n++) {
-
-				var c = string.charCodeAt(n);
-
-				if (c < 128) {
-					utftext += String.fromCharCode(c);
-				}
-				else if((c > 127) && (c < 2048)) {
-					utftext += String.fromCharCode((c >> 6) | 192);
-					utftext += String.fromCharCode((c & 63) | 128);
-				}
-				else {
-					utftext += String.fromCharCode((c >> 12) | 224);
-					utftext += String.fromCharCode(((c >> 6) & 63) | 128);
-					utftext += String.fromCharCode((c & 63) | 128);
-				}
-
-			}
-
-			return utftext;
-		},
-
-		// private method for UTF-8 decoding
-		_utf8_decode : function (utftext) {
-			var string = "";
-			var i = 0;
-			var c = c1 = c2 = 0;
-
-			while ( i < utftext.length ) {
-
-				c = utftext.charCodeAt(i);
-
-				if (c < 128) {
-					string += String.fromCharCode(c);
-					i++;
-				}
-				else if((c > 191) && (c < 224)) {
-					c2 = utftext.charCodeAt(i+1);
-					string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
-					i += 2;
-				}
-				else {
-					c2 = utftext.charCodeAt(i+1);
-					c3 = utftext.charCodeAt(i+2);
-					string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
-					i += 3;
-				}
-
-			}
-
-			return string;
-		}
-
-	}
-	
 	function base64dataURLencode(s) {
 		var b64 = "data:image/svg+xml;base64,";
 
+		// https://developer.mozilla.org/en/DOM/window.btoa
 		if (window.btoa) {
-			debug("using btoa for base64 encoding");
+			debug("using window.btoa for base64 encoding");
 			b64 += btoa(s);
 		} else {
-			debug("using custom base64 encoding");
+			debug("using custom base64 encoder");
 			b64 += Base64.encode(s);
 		}
 		
 		return b64;
 	}
-	
-	// WON'T WORK; canvas' origin-clean is dirty
+
 	function exportPNG() {
 		var canvas = document.createElement("canvas");
 		var ctx = canvas.getContext('2d');
+
+		// TODO: if (options.keepOutsideViewport), do some translation magic?
+
 		var img = new Image();
 		var svg_xml = XMLSerialize(_svg);
 		img.src = base64dataURLencode(svg_xml);
 
 		img.onload = function() {
+			debug("exported image size: " + [img.width, img.height])
+			canvas.width = img.width;
+			canvas.height = img.height;
 			ctx.drawImage(img, 0, 0);
+
 			// SECURITY_ERR WILL HAPPEN NOW
 			var png_dataurl = canvas.toDataURL();
-			callback(png_dataurl);
+			debug(type + " length: " + png_dataurl.length);
+
+			if (options.callback) options.callback( png_dataurl );
+			else debug("NOTE: no callback set, so nothing happens.");
 		}
 		
 		img.onerror = function() {
@@ -252,51 +139,79 @@ SVGElement.prototype.toDataURL = function(type, keepNonSafe, keepOutsideViewport
 			);
 		}
 
-		// TODO: will not return anything
+		// NOTE: will not return anything
 	}
 
 	function exportPNGcanvg() {
 		var canvas = document.createElement("canvas");
 		var ctx = canvas.getContext('2d');
-
-		// TODO: canvg issue don't require parentNode
-		canvas.setAttribute("style", "display: none;");
-		document.body.appendChild(canvas);
-
 		var svg_xml = XMLSerialize(_svg);
 
-		canvg(canvas, svg_xml, { ignoreMouse: true, ignoreAnimation: true });
-		var png_dataurl = canvas.toDataURL();
-		document.body.removeChild(canvas);
+		// NOTE: canvg gets the SVG element dimensions incorrectly if not specified as attributes
+		//debug("detected svg dimensions " + [_svg.clientWidth, _svg.clientHeight])
+		//debug("canvas dimensions " + [canvas.width, canvas.height])
 
-		return png_dataurl;
+		var keepBB = options.keepOutsideViewport;
+		if (keepBB) var bb = _svg.getBBox();
+
+		// NOTE: this canvg call is synchronous and blocks
+		canvg(canvas, svg_xml, { 
+			ignoreMouse: true, ignoreAnimation: true,
+			offsetX: keepBB ? -bb.x : undefined, 
+			offsetY: keepBB ? -bb.y : undefined,
+			scaleWidth: keepBB ? bb.width+bb.x : undefined,
+			scaleHeight: keepBB ? bb.height+bb.y : undefined,
+			renderCallback: function() {
+				debug("exported image dimensions " + [canvas.width, canvas.height]);
+				var png_dataurl = canvas.toDataURL();
+				debug(type + " length: " + png_dataurl.length);
+	
+				if (options.callback) options.callback( png_dataurl );
+			}
+		});
+
+		// NOTE: return in addition to callback
+		return canvas.toDataURL();
 	}
 
 	// BEGIN MAIN
 
 	if (!type) type = "image/svg+xml";
+	if (!options) options = {};
 
-	if (keepOutsideViewport) debug("keepOutsideViewport NOT supported and will be ignored!");
-	if (keepNonSafe) debug("keepNonSafe is NOT supported and will be ignored!");
+	if (options.keepNonSafe) debug("NOTE: keepNonSafe is NOT supported and will be ignored!");
+	if (options.keepOutsideViewport) debug("NOTE: keepOutsideViewport is only supported with canvg exporter.");
 	
 	switch (type) {
 		case "image/svg+xml":
-			return exportSVG(this);
+			return exportSVG();
 			break;
 
 		case "image/png":
 
-			if (window.canvg) {
-				debug("Using canvg for png exporting")
-				return exportPNGcanvg(this);
-			} else {
-				debug("Sorry! You don't have canvg. Using native hack for png exporting, THIS WILL FAIL!")
-				return exportPNG(this);
+			if (!options.renderer) {
+				if (window.canvg) options.renderer = "canvg";
+				else options.renderer="native";
+			}
+
+			switch (options.renderer) {
+				case "canvg":
+					debug("using canvg renderer for png export");
+					return exportPNGcanvg();
+					break;
+
+				case "native":
+					debug("using native renderer for png export. THIS WILL PROBABLY FAIL.");
+					return exportPNG();
+					break;
+
+				default:
+					debug("unknown png renderer given, doing noting (" + options.renderer + ")");
 			}
 
 			break;
 
 		default:
-			debug("Sorry! Exporting as \"" + type + "\" is not supported!")
+			debug("Sorry! Exporting as '" + type + "' is not supported!")
 	}
 }
